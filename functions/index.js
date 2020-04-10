fd2ab2eb58eb22fd56593916c7830492b3622ee1
dongsubak <likeap@catholic.ac.kr>
2020-04-10 18:47:40 +0900

    scream push

--- functions/index.js
+++ functions/index.js
@@ -13,11 +13,37 @@ app.get('/screams', (req, res) => {
         .then((data) => {
             let screams = [];
             data.forEach(doc => {
-                screams.push(doc.data());
+                // screams.push(doc.data());
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                })
             });
             return res.json(screams);
         })
         .catch(err => console.error(err));
+});
+
+app.post('/scream', (req, res) => {
+
+   const newScream = {
+       body: req.body.body,
+       userHandle: req.body.userHandle,
+       createdAt: admin.firestore.Timestamp.fromDate(new Date())
+   };
+
+   admin.firestore()
+       .collection('screams')
+       .add(newScream)
+       .then(doc => {
+           res.json({ message: `document ${doc.id} created successfully.`})
+       })
+       .catch(err => {
+           res.status(500).json({ error: 'something went wrong'});
+           console.error(err);
+       });
 });
 /*
 exports.helloWorld = functions.https.onRequest((request, response) => {
@@ -37,7 +63,7 @@ exports.getScreams = functions.https.onRequest((req, res) => {
         .catch(err => console.error(err));
  })
  */
-
+/*
 exports.createScream = functions.https.onRequest((req, res) => {
      if(req.method !== 'POST') {
          return res.status(400).json({ error: 'Method not allowed' });
@@ -59,7 +85,7 @@ exports.createScream = functions.https.onRequest((req, res) => {
             console.error(err);
         });
  });
-
+*/
  // https://baseurl.com/api/
 
  exports.api = functions.https.onRequest(app);
