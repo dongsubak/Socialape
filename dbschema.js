49f97cbb07433bbdd88dcdfd3df4a781778df6f9
dongsubak <likeap@catholic.ac.kr>
2020-04-14 16:14:50 +0900

    dbschema

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-04-14T07:13:33.694Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
